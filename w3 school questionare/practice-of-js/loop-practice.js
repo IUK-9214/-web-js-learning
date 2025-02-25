e7dd340c9ad0bb04fd11ef6020d2093b676b1cd0
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-02-25 18:42:39 +0500

    add more info
    here i also use the for of loop

--- w3 school questionare/practice-of-js/loop-practice.js
+++ w3 school questionare/practice-of-js/loop-practice.js
@@ -1,10 +1,19 @@
-console.log("this is the loop practice");
-let details={
-    name :"ibad",
-    role :"programer",
-    company:"codercando"
-}
-for (const i in details){
-let elemnet=details[i];
-console.log(i,elemnet);
+//user use for in loop for the objects
+
+// console.log("this is the loop practice");
+// let details={
+//     name :"ibad",
+//     role :"programer",
+//     company:"codercando"
+// }
+// for (const i in details){
+// let elemnet=details[i];
+// console.log(i,elemnet);
+// }
+
+// for in loop use for the arrray most of the time 
+console.log("for of loop now ");
+let h="harray";
+for(let c of h){
+    console.log(c);
 }
