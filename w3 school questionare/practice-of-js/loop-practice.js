f455e644734481a6b12f3c5716674171bf18af8b
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-02-25 18:35:58 +0500

    for in loop
    foe in loop with the object used here

---
+++ w3 school questionare/practice-of-js/loop-practice.js
@@ -0,0 +1,10 @@
+console.log("this is the loop practice");
+let details={
+    name :"ibad",
+    role :"programer",
+    company:"codercando"
+}
+for (const i in details){
+let elemnet=details[i];
+console.log(i,elemnet);
+}
