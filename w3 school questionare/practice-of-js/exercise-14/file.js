734764e0bdb1ef4870564b63ee5a06aeb0f1f52b
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-03-19 14:10:34 +0500

    still working

--- w3 school questionare/practice-of-js/exercise-14/file.js
+++ w3 school questionare/practice-of-js/exercise-14/file.js
@@ -1,10 +1,54 @@
-let btun=document.getElementById("btn")
-btun.addEventListener("click",()=>{
-alert("Your pc is hacked")
-let item=document.createElement("div")
-item.setAttribute("class","hacking");
-item.style.backgroundColor="green";
-item.innerHTMLL="this is my insterted";
-item.style.color="white";
-document.getElementsByClassName(".hacker").appendchild(item);
-})
+
+function create_p(texte){
+    let item =document.createElement("p");
+    item.style.background="black";
+    item.style.color="green";
+    item.innerHTML=texte;
+    item.setAttribute("class","inserted")
+    document.getElementById("hk").appendChild(item)
+    }
+
+let data = setTimeout(()=>{
+let item = document.getElementById("hk");
+item.style.background="black";
+item.style.color="green";
+item.innerHTML="Hacking initilaizing";
+
+let obj=[
+    "Reading your files ","Password files detected ","Sending all Passwords and Personal files to server ","Cleaning up "
+]
+
+let i=0;
+    let intr=setInterval(()=>{
+create_p(obj[i])
+let count=0;
+   let inter= setInterval(()=>{
+        item.textContent+=".";
+        count+=1;
+        if(count==4){
+          clearInterval(inter)
+        }
+    },500)
+    
+i++;
+
+if(i==4){
+clearInterval(intr)
+}
+
+    },2000)
+    
+
+
+   let count=0;
+   let inter= setInterval(()=>{
+        item.textContent+=".";
+        count+=1;
+        if(count==4){
+          clearInterval(inter)
+        }
+    },500)
+
+
+}
+,2000)
