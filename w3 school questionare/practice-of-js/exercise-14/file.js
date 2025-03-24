dcb94ac3c7c7b8a2533c942ca97dc8c175d22549
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-03-24 10:44:47 +0500

    finally sorted out the problem

--- w3 school questionare/practice-of-js/exercise-14/file.js
+++ w3 school questionare/practice-of-js/exercise-14/file.js
@@ -1,51 +1,49 @@
-
-function create_p(texte){
-    let item =document.createElement("p");
-    item.style.background="black";
-    item.style.color="green";
-    item.innerHTML=texte;
-    item.setAttribute("class","inserted")
-    document.getElementById("hk").appendChild(item)
-    }
-
-let data = setTimeout(()=>{
-let item = document.getElementById("hk");
-item.style.background="black";
-item.style.color="green";
-item.innerHTML="Hacking initilaizing ";
-
-let obj=[
-    "Reading your files ","Password files detected ","Sending all Passwords and Personal files to server ","Cleaning up "
-]
-
-let i=0;
-    let intr=setInterval(()=>{
-create_p(obj[i])
-let count=0;
-   let inter= setInterval(()=>{
-        item.textContent+=".";
-        count+=1;
-        if(count==4){
-          clearInterval(inter)
-        }
-    },400)
-    
-i++;
-
-if(i==4){
-clearInterval(intr)
+function create_p(texte) {
+    let item = document.createElement("p");
+    item.style.background = "black";
+    item.style.color = "green";
+    item.innerHTML = texte;
+    item.setAttribute("class", "inserted");
+    document.getElementById("hk").appendChild(item);
 }
 
-    },2000)
-    
-   let count=0;
-   let inter= setInterval(()=>{
-        item.textContent+=".";
-        count+=1;
-        if(count==4){
-          clearInterval(inter)
-        }
-    },500)
+let data = setTimeout(() => {
+    let item = document.getElementById("hk");
+    item.style.background = "black";
+    item.style.color = "green";
+    item.innerHTML = "";  
 
-}
-,2000)
+    let obj = [
+        "Hacking initializing",
+        "Reading your files",
+        "Password files detected",
+        "Sending all Passwords and Personal files to server",
+        "Cleaning up"
+    ];
+
+    let i = 0;
+
+    let intr = setInterval(() => {
+        let newLine = document.createElement("p"); 
+        newLine.style.color = "green";
+        newLine.textContent = obj[i]; 
+        document.getElementById("hk").appendChild(newLine); 
+
+        let count = 0;
+        let inter = setInterval(() => {
+            newLine.textContent += "."; 
+            count += 1;
+            if (count == 4) {
+                clearInterval(inter);
+            }
+        }, 400);
+
+        i++;
+
+        if (i == obj.length) {
+            clearInterval(intr);
+        }
+
+    }, 2000);
+
+}, 2000);
